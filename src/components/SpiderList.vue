5abdc967b58adaf9d312baef31780d6214ebbc37
刘博伦 <liubolunliuyao@gmail.com>
2018-07-10 11:10:17 +0800

    fist step

--- src/components/SpiderList.vue
+++ src/components/SpiderList.vue
@@ -5,39 +5,41 @@
     style="width: 100%">
     <el-table-column
       fixed
-      prop="date"
-      label="日期"
-      width="150">
+      prop="site.domain"
+      label="域名"
+      width="200">
     </el-table-column>
     <el-table-column
-      prop="name"
-      label="姓名"
+      prop="threadNum"
+      label="线程"
       width="120">
     </el-table-column>
     <el-table-column
-      prop="province"
-      label="省份"
+      prop="interval"
+      label="间隔"
       width="120">
     </el-table-column>
     <el-table-column
-      prop="city"
-      label="市区"
-      width="120">
+      prop="run"
+      label="是否运行"
+      width="120"
+      :formatter="boolRunForMatter">
     </el-table-column>
     <el-table-column
-      prop="address"
-      label="地址"
-      width="300">
+      prop="on"
+      label="是否启动"
+      width="120"
+      :formatter="boolOnForMatter">
     </el-table-column>
     <el-table-column
-      prop="zip"
-      label="邮编"
-      width="120">
+      prop="lastTime"
+      label="上次运行时间"
+      width="120"
+      :formatter="timeForMatter">
     </el-table-column>
     <el-table-column
       fixed="right"
-      label="操作"
-      width="100">
+      label="操作">
       <template slot-scope="scope">
         <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
         <el-button type="text" size="small">编辑</el-button>
@@ -54,9 +56,33 @@
       }
     },
 
+    created() {
+      this.showTeacherList()
+    },
+
+    methods: {
+      showTeacherList () {
+        this.$axios.get(this.$url + "/siteTask")
+          .then(response => {
+            if (response.data.code === 0) {
+              this.tableData = response.data.result
+            }
+          })
+      },
+      boolOnForMatter(row, column) {
+        return row.on ? "是" : "否"
+      },
+      boolRunForMatter(row, column) {
+        return row.run ? "是" : "否"
+      },
+      timeForMatter(row, column) {
+        return this.$tools.fmtDate(row.lastTime * 1000)
+      }
+    },
+
     data() {
       return {
-        tableData: this.$axios.get()
+        tableData: []
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import 'babel-polyfill'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui';
 import axios from 'axios'
+import tools from '@/tools'
 
+const url = "http://localhost:8080"
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
 Vue.prototype.$axios = axios
+Vue.prototype.$url = url
+
+Object.defineProperty(Vue.prototype, '$tools', {value: tools})
+
 
 new Vue({
   el: '#app',
