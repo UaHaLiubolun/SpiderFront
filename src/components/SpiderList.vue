854f5c2c717e5d46336827ca20f0c1249de9a133
刘博伦 <liubolunliuyao@gmail.com>
2018-07-10 18:58:18 +0800

    爬虫页面

--- src/components/SpiderList.vue
+++ src/components/SpiderList.vue
@@ -1,58 +1,268 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      fixed
-      prop="site.domain"
-      label="域名"
-      width="200">
-    </el-table-column>
-    <el-table-column
-      prop="threadNum"
-      label="线程"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="interval"
-      label="间隔"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="run"
-      label="是否运行"
-      width="120"
-      :formatter="boolRunForMatter">
-    </el-table-column>
-    <el-table-column
-      prop="on"
-      label="是否启动"
-      width="120"
-      :formatter="boolOnForMatter">
-    </el-table-column>
-    <el-table-column
-      prop="lastTime"
-      label="上次运行时间"
-      width="120"
-      :formatter="timeForMatter">
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作">
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-        <el-button type="text" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div>
+      <el-button type="primary" @click="addInfo()">添加</el-button>
+    </div>
+    <div>
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          fixed
+          prop="site.domain"
+          label="域名"
+          width="200">
+        </el-table-column>
+        <el-table-column
+          prop="threadNum"
+          label="线程"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="interval"
+          label="间隔"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="run"
+          label="是否正在运行"
+          width="120"
+          :formatter="boolRunForMatter">
+        </el-table-column>
+        <el-table-column
+          prop="on"
+          label="是否启动"
+          width="120"
+          :formatter="boolOnForMatter">
+        </el-table-column>
+        <el-table-column
+          prop="lastTime"
+          label="上次运行时间"
+          width="150"
+          :formatter="timeForMatter">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作">
+          <template slot-scope="scope">
+            <el-button @click="viewInfo(scope.row)" type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div>
+      <el-dialog
+        title="详细信息"
+        :visible.sync="infoDialog"
+        width="80%">
+        <el-form ref="siteTask" :model="siteTask" >
+          <el-form-item label="域名">
+            <el-input v-model="siteTask.site.domain"></el-input>
+          </el-form-item>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item label="线程数">
+                <el-input v-model="siteTask.threadNum"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="2">&nbsp;</el-col>
+            <el-col :span="10">
+              <el-form-item label="间隔">
+                <el-input v-model="siteTask.interval"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-form-item label="是否启用">
+            <el-switch v-model="siteTask.on"></el-switch>
+          </el-form-item>
+
+          <el-collapse v-model="activeNames">
+            <el-collapse-item title="域名信息" name="1">
+              <el-form-item label="域名">
+                <el-input v-model="siteTask.site.domain"></el-input>
+              </el-form-item>
+            </el-collapse-item>
+            <el-collapse-item title="StartURL" name="2">
+              <el-form-item
+                v-for="(url, index) in startUrlTran"
+                :label="'URL-' + index"
+                :key="url.key"
+                :prop="'startUrlTran.' + index + '.value'"
+                :rules="{
+      required: true, message: 'Url不能为空', trigger: 'blur'
+    }"
+              >
+                <el-input v-model="url.value"></el-input><el-button @click.prevent="removeStartUrl(url)">删除</el-button>
+              </el-form-item>
+              <el-form-item>
+                <el-button @click="addStartUrl">新增Url</el-button>
+              </el-form-item>
+            </el-collapse-item>
+            <el-collapse-item title="PageModels" name="3">
+              <el-row>
+                <el-col :span="22" :offset="1">
+                  <el-collapse v-model="activeCNames">
+                    <el-collapse-item title="TargetUrl" name="31">
+                      <el-form-item
+                        v-for="(url, index) in targetUrlTran"
+                        :label="'TargetURL-' + index"
+                        :key="url.key"
+                        :prop="'targetUrlTran.' + index + '.value'"
+                        :rules="{
+      required: true, message: 'Url不能为空', trigger: 'blur'
+    }"
+                      >
+                        <el-input v-model="url.value"></el-input><el-button @click.prevent="removeTargetUrl(url)">删除</el-button>
+                      </el-form-item>
+                      <el-form-item>
+                        <el-button @click="addTargetUrl">新增TargetUrl</el-button>
+                      </el-form-item>
+                    </el-collapse-item>
+
+                    <el-collapse-item title="HelpUrl" name="32">
+                      <el-form-item
+                        v-for="(url, index) in helpUrlTran"
+                        :label="'HelpURL-' + index"
+                        :key="url.key"
+                        :prop="'helpUrlTran.' + index + '.value'"
+                        :rules="{
+      required: true, message: 'Url不能为空', trigger: 'blur'
+    }"
+                      >
+                        <el-input v-model="url.value"></el-input><el-button @click.prevent="removeHelpUrl(url)">删除</el-button>
+                      </el-form-item>
+                      <el-form-item>
+                        <el-button @click="addHelpUrl">新增HelpUrl</el-button>
+                      </el-form-item>
+                    </el-collapse-item>
+
+                    <el-collapse-item title="Extracts" name="33">
+                      <el-form-item
+                        v-for="(extract, index) in siteTask.pageModels[0].extracts"
+                      >
+                        <el-row>
+                          <el-col :span="5">
+                            <el-form-item label="字段">
+                              <el-input v-model="extract.filed"></el-input>
+                            </el-form-item>
+                          </el-col>
+                          <el-col :span="1">&nbsp;</el-col>
+                          <el-col :span="4">
+                            <el-form-item label="数据类型">
+                              <el-select v-model="extract.source" placeholder="数据类型">
+                                <el-option label="Html" value="Html"></el-option>
+                                <el-option label="Json" value="Json"></el-option>
+                              </el-select>
+                            </el-form-item>
+                          </el-col>
+                          <el-col :span="1">&nbsp;</el-col>
+                          <el-col :span="4">
+                            <el-form-item label="解析方式">
+                              <el-select v-model="extract.type" placeholder="解析方式">
+                                <el-option label="Xpath" value="Xpath"></el-option>
+                                <el-option label="Regex" value="Regex"></el-option>
+                              </el-select>
+                            </el-form-item>
+                          </el-col>
+                          <el-col :span="1">&nbsp;</el-col>
+                          <el-col :span="3">
+                            <el-form-item label="NotNull">
+                              <el-switch v-model="extract.notNull"></el-switch>
+                            </el-form-item>
+                          </el-col>
+                          <el-col :span="1">&nbsp;</el-col>
+                          <el-col :span="3">
+                            <el-form-item label="是否多项">
+                              <el-switch v-model="extract.multi"></el-switch>
+                            </el-form-item>
+                          </el-col>
+                        </el-row>
+                        <el-form-item label="表达式">
+                          <el-input v-model="extract.value"></el-input>
+                        </el-form-item>
+                        <el-button @click.prevent="removeExtract(extract)">删除</el-button>
+                      </el-form-item>
+                      <el-form-item>
+                        <el-button @click="addExtract">新增HelpUrl</el-button>
+                      </el-form-item>
+                    </el-collapse-item>
+                  </el-collapse>
+                </el-col>
+              </el-row>
+            </el-collapse-item>
+          </el-collapse>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="submitData">保存</el-button>
+          <el-button @click="infoDialog = false">取 消</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    methods: {
-      handleClick(row) {
-        console.log(row);
+    data() {
+      return {
+        infoDialog: false,
+        action: "edit",
+        activeNames: [],
+        activeCNames: [],
+        startUrlTran: [{
+          value: "",
+          key: new Date()
+        }],
+        targetUrlTran: [{
+          value: "",
+          key: new Date()
+        }],
+        helpUrlTran: [{
+          value: "",
+          key: new Date()
+        }],
+        siteTask: {
+          site:{
+            domain: "",
+            userAgent: null,
+            cookies: {},
+            charset: "utf-8",
+            sleepTime: 0,
+            retryTimes: 0,
+            cycleRetryTimes: 0,
+            retrySleepTime: 1000,
+            timeOut: 5000,
+            acceptStatCode: [
+              200
+            ],
+            headers: {},
+          },
+          startUrls: [],
+          threadNum: 0,
+          interval: 0,
+          pageModels: [{
+            targetUrl: [],
+            helpUrl: [],
+            extracts: [{
+              filed: "",
+              value: "",
+              type: "",
+              source: "",
+              notNull: false,
+              multi: false,
+              dataConversion: {
+                function: "",
+                expression: ""
+              }
+            }]
+          }],
+          lastTime: 0,
+          on: false,
+          run: false
+        },
+        tableData: []
       }
     },
 
@@ -77,12 +287,108 @@
       },
       timeForMatter(row, column) {
         return this.$tools.fmtDate(row.lastTime * 1000)
-      }
-    },
-
-    data() {
-      return {
-        tableData: []
+      },
+      viewInfo(row) {
+        this.infoDialog = true
+        this.action = "edit"
+        this.siteTask = row
+        this.startUrlTran = this.tranUrl(row.startUrls)
+        this.targetUrlTran = this.tranUrl(row.pageModels[0].targetUrl)
+        this.helpUrlTran = this.tranUrl(row.pageModels[0].helpUrl)
+        console.log(row)
+      },
+      addInfo() {
+        this.action = "add"
+        this.infoDialog = true
+      },
+      submitData() {
+        this.siteTask.startUrls = this.collectUrl(this.startUrlTran)
+        this.siteTask.pageModels[0].targetUrl = this.collectUrl(this.targetUrlTran)
+        this.siteTask.pageModels[0].helpUrl = this.collectUrl(this.helpUrlTran)
+        if (this.action === "add") {
+          this.$axios.post(this.$url + "/siteTask", this.siteTask)
+            .then(res => {
+              if (res.data.code === 0) {
+                this.infoDialog = false
+                this.showTeacherList()
+              } else {
+                alert("failed")
+              }
+            })
+        }
+      },
+      addStartUrl() {
+        this.startUrlTran.push({
+          value: "",
+          key: new Date()
+        })
+      },
+      removeStartUrl(url) {
+        var index = this.startUrlTran.indexOf(url)
+        if (index !== -1) {
+          this.startUrlTran.splice(index, 1)
+        }
+      },
+      addHelpUrl() {
+        this.helpUrlTran.push({
+          value: "",
+          key: new Date()
+        })
+      },
+      removeHelpUrl(url) {
+        var index = this.helpUrlTran.indexOf(url)
+        if (index !== -1) {
+          this.helpUrlTran.splice(index, 1)
+        }
+      },
+      addTargetUrl() {
+        this.targetUrlTran.push({
+          value: "",
+          key: new Date()
+        })
+      },
+      removeTargetUrl(url) {
+        var index = this.targetUrlTran.indexOf(url)
+        if (index !== -1) {
+          this.targetUrlTran.splice(index, 1)
+        }
+      },
+      addExtract() {
+        this.siteTask.pageModels[0].extracts.push({
+          filed: "",
+          value: "",
+          type: "Xpath",
+          source: "Html",
+          notNull: false,
+          multi: false,
+          dataConversion: {
+            function: "",
+            expression: ""
+          }
+        })
+      },
+      removeExtract(url) {
+        var index = this.siteTask.pageModels[0].extracts(url)
+        if (index !== -1) {
+          this.siteTask.pageModels[0].extracts.splice(index, 1)
+        }
+      },
+      collectUrl(urlTran) {
+        var urls = []
+        for (var i in urlTran) {
+          urls.push(urlTran[i].value)
+        }
+        return urls
+      },
+      tranUrl(urls) {
+        var urlTran = []
+        for (var i in urls) {
+          urlTran.push({
+            value: urls[i],
+            key: Date.now()
+          })
+        }
+        return urlTran
       }
     }
   }
--- src/tools.js
+++ src/tools.js
@@ -5,6 +5,8 @@ export default {
     var y = 1900 + date.getYear()
     var m = '0' + (date.getMonth() + 1)
     var d = '0' + date.getDate()
-    return y + '-' + m.substring(m.length - 2, m.length) + '-' + d.substring(d.length - 2, d.length)
+    var h = '0' + date.getHours()
+    var ms = '0' + date.getMinutes()
+    return y + '-' + m.substring(m.length - 2, m.length) + '-' + d.substring(d.length - 2, d.length) + ' ' + h.substring(h.length - 2, h.length) + ':' + ms.substring(ms.length - 2, ms.length)
   },
 }
